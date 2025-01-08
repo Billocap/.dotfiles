058481caed95737280bf047ea695e50e2d7dcce2
Gabriel Quintino <pikachurando@protonmail.com>
2025-01-08 15:14:52 -0300

    ref: luarocks installation

--- modules/lua/init.sh
+++ modules/lua/init.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-# Installs C related deps
+# Installs lua
 . $MODULE_FOLDER/$SELECTED_DISTRO/install.sh
+
+# Installs luarocks
+wget https://luarocks.org/releases/luarocks-3.11.1.tar.gz
+
+tar zxpf $WORKDIR/luarocks-3.11.1.tar.gz
+
+cd $WORKDIR/luarocks-3.11.1
+
+bash ./configure && make && sudo make install
+
+cd $WORKDIR
 
