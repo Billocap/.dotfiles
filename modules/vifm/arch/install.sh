793bf3a6ca2770ac0b7ad043debba7d411216017
Gabriel Quintino <pikachurando@protonmail.com>
2025-01-08 11:33:10 -0300

    dep: vifm update

--- modules/vifm/arch/install.sh
+++ modules/vifm/arch/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -Sy vifm
+sudo pacman -Sy tar
--- modules/vifm/init.sh
+++ modules/vifm/init.sh
@@ -5,8 +5,20 @@
 # Necessary deps
 . $MODULE_FOLDER/$SELECTED_DISTRO/install.sh
 
+# Builds Vifm
+tar -xvjf $MISC_FOLDER/vifm-0.13.tar.bz2
+
+sudo chmod -R ugo+rwx $WORKDIR/vifm-0.13
+
+cd $WORKDIR/vifm-0.13
+
+sudo . configure && sudo make && sudo make install
+
+cd $WORKDIR
+
 # Creates the configuration folder for vifm
 vifm -c q
 
 sudo cp -r $CONFIGS_FOLDER/vifm/* $WORKDIR/.config/vifm
 
+sudo chmod -R ugo+rw $WORKDIR/.config/vifm
